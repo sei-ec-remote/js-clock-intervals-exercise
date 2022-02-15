ab7c83d76692ff4c8fa4350e00eac1ceff0dd349
oliviawilcox1 <oliviawilcox07@gmail.com>
2022-02-14 21:14:47 -0500

    clock intervals

---
+++ clock.js
@@ -0,0 +1,32 @@
+const sec = document.getElementById('second');
+const min = document.getElementById('minute');
+const hour = document.getElementById('hour');
+// min.style.transform = "rotate(" + minuteRotations + "deg)"
+// sec.style.transform = "rotate(" + secondRotations + "deg)"
+// sec.style.transform = "rotate(" + hourRotations + "deg)"
+
+
+
+const countSec = setInterval(() => {
+  
+    const todaysTime = new Date()
+    console.log(`This is todays time:`, todaysTime);
+    const hourRotations = todaysTime.getHours()/12 * 360 - 90;
+    
+    const secondRotations = todaysTime.getSeconds()/60 * 360 - 90;
+     
+    const minuteRotations = todaysTime.getMinutes()/60 * 360 - 90;
+
+    min.style.transform = "rotate(" + minuteRotations + "deg)"
+   console.log(minuteRotations)
+    sec.style.transform = "rotate(" + secondRotations + "deg)"
+    console.log(secondRotations)
+    hour.style.transform = "rotate(" + hourRotations + "deg)"
+    console.log(hourRotations)
+
+}, 1000)
+
+
+
+
+
