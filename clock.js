e202ea72b35d9e654de0a050410526a005633268
dnierman0920 <davidnierman93@gmail.com>
2022-02-17 16:46:10 -0800

    Create function to roate clock hands & const functions for each hand to know degree of rotation

--- clock.js
+++ clock.js
@@ -3,5 +3,24 @@ const hourHand = document.getElementById('hour')
 const minuteHand = document.getElementById('minute')
 const secondHand = document.getElementById('second')
 
+// create a funcion that will rotate the clock hands
+// given degrees and hands as parameters
+const rotateDegrees = (hand,degrees) => {
+    hand.style.transform = `rotate(${degrees}deg)`
+}
+
+// create constant variables for degrees rotation every second
+// second hand --> full rotation has 60 moves
+// second hand --> there are 360 degrees in a circle
+const secondDegrees = i => (i/60) * 360
+
+// minute hand --> full rotation has 60 moves
+// minute hand --> there are 360 degrees in a circle
+const secondDegrees = i => (i/60) * 360
+
+// hour hand --> full rotation is 12 moves
+// hour hand --> there are 360 degrees in a circle
+const hourDegrees = i => (i/12) * 360
+
 
 
