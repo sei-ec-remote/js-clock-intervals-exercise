40d67ee2c70eb6a2db47cc2e589a8bab2131fbe9
dnierman0920 <davidnierman93@gmail.com>
2022-02-17 16:21:08 -0800

    define reference variables to each of the clock hands

--- clock.js
+++ clock.js
@@ -1,1 +1,7 @@
 // define reference variables to each of the hands
+const hourHand = document.getElementById('hour')
+const minuteHand = document.getElementById('minute')
+const secondHand = document.getElementById('second')
+
+
+
