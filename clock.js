05c71274b53539182f74a202b2162c326c42c144
lyee197 <liamsyee@gmail.com>
2022-03-29 23:46:17 -0400

    Finished second/min/hour hand

--- clock.js
+++ clock.js
@@ -1,65 +1,39 @@
-// make a afunction for seconds
-// for every second that goes by,
-// we want the hand to move 6 degrees
 // 1000 = 1 sec
 // 60000 = 60 secs = 1 min
 // 3600000 = 60 mins = 1 hour
-// const second = () => {
 
-// }
+// Grab the element from the document
+const secondHand = document.getElementById('second')
+const minHand = document.getElementById('minute')
+const hourHand = document.getElementById('hour')
+
+let degreesS = 1
+let degreesM = 1
+let degreesH = 1
+
+// make a afunction for seconds
+// for every second that goes by,
+// we want the hand to move 6 degrees
+seconds = setInterval(() => {
+    let secRotation = (degreesS/ 60) * 360
+    secondHand.style.transform = "rotate(" + secRotation + "deg)"
+    degreesS += 1
+}, 1000)
 
 // make a function for mins
 // for every 60 seconds that goes by,
 // we want the hand to move 6 degrees
-// const min = () => {
-
-// }
+minutes = setInterval(() => {
+    let minRotation = (degreesM/ 60) * 360
+    minHand.style.transform = "rotate(" + minRotation + "deg)" 
+    degreesM += 1
+}, 60000)
 
 // make a function for hours
 // for every 60 mins that goes by,
 // we want the hand to move 30 degrees
-// const hour = () => {
-
-// }
-let degrees = 0
-
-//increase the degree of the interval by 6 degrees by 1 second
-const countInt = setInterval(() => {
-    console.log(degrees)
-    degrees += 6
-    return degrees
-}, 1000)
-console.log(degrees)
-setTimeout(() => {
-    clearInterval(countInt)
-}, 60100)
-degrees += 6
-
-// const countMin = setInterval(() => {
-//     console.log
-// }
-// )
-
-// const sec = () => {
-//     const el = document.getElementById("second")
-//     el.style.transform = "rotate(" + countInt + "deg)"
-//     console.log(`rotate(" + ${countInt} + "deg)`)
-// }
-// const sixRotation = setTimeout(sec, 1000)
-
-//rotates the second hand by 6 degrees every second from const countInt
-function secondRotation(degrees) {
-    const ap = document.getElementById('second')
-    ap.style.transform = "rotate(" + degrees + "deg)"
-}
-
-secondRotation(countInt)
-
-// gets the id='second' and changes the angle at which 
-// the second hand is at
-
-const secondHand = document .getElementById('second')
-const minHand = document.getElementById('minute')
-const hourHand = document.getElementById('hour')
-
-let x = secounds/60*360
+hours = setInterval(() => {
+    let hourRotation = (degreesH/ 12) * 360
+    hourHand.style.transform = "rotate(" + hourRotation + "deg)"
+    degreesH += 1
+}, 360000)
