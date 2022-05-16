d645012cd2654ddfda754a2456ccfcd2e957118b
bhenderson93 <brycehenderson1993@gmail.com>
2022-05-16 15:51:34 -0700

    Clock is functional and initializes to current time (at least for my timezone)

--- clock.js
+++ clock.js
@@ -1,11 +1,13 @@
 //This is my grandfather clock
 
-const rotateImg = (imgID, degrees) => {
+//function to rotate images by a set amount when called.
+const rotateImg = (imgID) => {
 
     //rotational degree values
     let normalRot = 6
     let hourRot = 360 / (12 * 60)
     let myImg = document.getElementById(imgID)
+
     //stuff for second hand
     if (myImg.id === 'second') {
         secPos += normalRot
@@ -28,6 +30,7 @@ const rotateImg = (imgID, degrees) => {
         }
         console.log('Bing!')
         myImg.style.transform = `translate(-50%, -50%) rotate(${minPos}deg)`
+
         //stuff for the hour hand
     } else {
         hourPos += hourRot
@@ -41,15 +44,12 @@ const rotateImg = (imgID, degrees) => {
                 setTimeout(console.log("BOOOOOOONG.........."), 1500)
             }
         }
-
         myImg.style.transform = `translate(-50%, -50%) rotate(${hourPos}deg)`
     }
-
-
-
 }
 
 const myClock = () => {
+    //function to be called that counts up and calls the rotate image function for clock adjustment
     const countUp = () => {
         second++
         rotateImg('second', secPos)
@@ -57,17 +57,34 @@ const myClock = () => {
             rotateImg('minute', minPos)
             rotateImg('hour', hourPos)
         }
-
     }
 
     //If having fun, reduce the second parameter in the method below accordingly. (ie, when working on clock coding projects, set to 5)
     setInterval(countUp, 1000)
 }
 
+//function to fetch initial date/time and set the clock
 const setInitialTime = () => {
+    let rightNow = new Date()
+    console.log( rightNow.getHours(),rightNow.getMinutes(),rightNow.getSeconds())
 
+    //I subtract 1 from each value being constructed when calculating hand positions because the rotate image function adds an increment.
+
+    //construct minutes
+    let currMin = rightNow.getMinutes()
+    minPos = (currMin-1)*6
+    rotateImg('minute')
+
+    //construct hours
+    let currHour = rightNow.getHours()%12
+    hourPos = (currHour*30)+((currMin-1)*0.5)
+    rotateImg('hour')
+
+    //construct seconds
+    second = rightNow.getSeconds()
+    secPos = (second-1)*6
+    rotateImg('second')
 }
-
 
 //rotational positions of the hands
 let secPos = 0
@@ -76,6 +93,8 @@ let hourPos = 0
 
 //Set seconds to 0
 let second = 0
-let rightNow = new Date()
+
 //call the function to start it running
+//myClock()
+setInitialTime()
 myClock()
