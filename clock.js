eb0b8c8cdd99f56fabc0f41f4778219a1632fcf0
patwyzlic <patrickwyzlic@gmail.com>
2022-05-16 19:50:56 -0400

    Commit 2 - Inititate JS Changing Degrees

---
+++ clock.js
@@ -0,0 +1,11 @@
+let degrees = 45;
+
+const hourEl = document.getElementById("hour");
+hourEl.style.transform = "rotate(" + degrees + "deg)";
+
+const minuteEl = document.getElementById("minute");
+minuteEl.style.transform = "rotate(" + degrees + "deg)";
+
+const secondEl = document.getElementById("second");
+secondEl.style.transform = "rotate(" + degrees + "deg)";
+
