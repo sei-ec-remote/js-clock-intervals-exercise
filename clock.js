417bb695ff761010dc4f68c49f87ea53f0203c18
jhimson <jhimsonrayraboypamisa@gmail.com>
2022-05-16 17:29:42 -0400

    Create variables and store each image element parts of the clock

---
+++ clock.js
@@ -0,0 +1,6 @@
+// Getting the image element of seconds by id and storing it the variable seconds.
+const seconds = document.getElementById('second');
+// Getting the image element of minutes by id and storing it the variable minutes.
+const minutes = document.getElementById('minute');
+// Getting the image element of hour by id and storing it the variable hours.
+const hours = document.getElementById('hour');
