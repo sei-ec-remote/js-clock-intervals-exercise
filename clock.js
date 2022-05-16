53dd2aa34dcc27f2eeb22be8de7d49377b88fbc6
“kmcaloney15” <Katherine@Katys-MBP-2.hsd1.ma.comcast.net>
2022-05-16 17:57:05 -0400

    I set up the current time

---
+++ clock.js
@@ -0,0 +1,38 @@
+//okay lets write out what I want to code
+//we need to make the clock have an hour, minute and red second hand
+
+// Create intervals in the JavaScript file to make the clocks tick.
+
+
+
+// Change CSS values in JavaScript by getting reference to them through the DOM and setting a new value to their CSS properties:
+
+let degrees = 45;
+
+
+//pulling the hour info
+const hourElement = document.getElementById("hour")
+el.style.transform = "rotate(" + degrees + "deg)"
+
+// It would be useful to define functions that convert seconds, minutes and hours to how many degrees the hand should be rotated around the clock!
+
+// secondRotation(0) // returns 0
+// secondRotation(59) // returns (59 / 60) * 360
+// const seconds () => {
+
+// }
+
+
+// hourRotation(12) // returns 0
+// hourRotation(5) // returns (5 / 12) * 360
+
+
+currentTime = new Date();
+console.log(currentTime);
+
+
+
+
+
+
+// setInterval('clockRun');
