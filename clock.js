8141f19a0af74c19942fccb07bcba3152d694325
bhenderson93 <brycehenderson1993@gmail.com>
2022-05-16 16:53:50 -0700

    Rewrote the JavaScript to only take 11 lines

--- clock.js
+++ clock.js
@@ -1,103 +1,11 @@
-//This is my grandfather clock
-
-//function to rotate images by a set amount when called.
-const rotateImg = (imgID) => {
-
-    //rotational degree values
-    let normalRot = 6
-    let hourRot = 360 / (12 * 60)
-    let myImg = document.getElementById(imgID)
-
-    //stuff for second hand
-    if (myImg.id === 'second') {
-        secPos += normalRot
-        if (second % 2 === 1) {
-            console.log('Tick...')
-        } else {
-            console.log('Tock...')
-        }
-        if (secPos === 360) {
-            secPos = 0
-            second = 0
-            //Minute hand's about to move!
-            console.log('Bing!')
-        }
-        myImg.style.transform = `translate(-50%, -50%) rotate(${secPos}deg)`
-
-    //stuff for minute hand
-    } else if (myImg.id === 'minute') {
-        minPos += normalRot
-        if (minPos === 360) {
-            minPos = 0
-        }
-        myImg.style.transform = `translate(-50%, -50%) rotate(${minPos}deg)`
-
-    //stuff for the hour hand
-    } else {
-        hourPos += hourRot
-        if (hourPos === 720) {
-            hourPos = 0
-        }
-        //I've decided this is a grandfather clock.
-        if (hourPos % 30 === 0) {
-            let theHour = hourPos / 30
-            for (let i = 0; i < theHour; i++) {
-                const bong = () =>{
-                    console.log("BOOOOOOONGGGG....")
-                }
-                setTimeout(bong, 1500)
-            }
-        }
-        myImg.style.transform = `translate(-50%, -50%) rotate(${hourPos}deg)`
-    }
-}
-
-const myClock = () => {
-    //Figure out what time it is
-    setInitialTime()
-
-    //function to be called that counts up and calls the rotate image function for clock adjustment
-    const countUp = () => {
-        second++
-        rotateImg('second', secPos)
-        if (second % 60 === 0) {
-            rotateImg('minute', minPos)
-            rotateImg('hour', hourPos)
-        }
-    }
-
-    //If having fun, reduce the 2nd parameter in the method below proportionally in order to accurately represent the feeling of time's passage. (ie, when working on clock coding projects, set to 100)
-    setInterval(countUp, 1000)
-}
-
-//function to fetch initial date/time and set the clock
-const setInitialTime = () => {
-    let rightNow = new Date()
-
-    //I subtract 1 from each value being constructed when calculating hand positions because the rotate image function adds an increment.
-    //construct minutes
-    let currMin = rightNow.getMinutes()
-    minPos = (currMin-1)*6
-    rotateImg('minute')
-
-    //construct hours
-    let currHour = rightNow.getHours()%12
-    hourPos = (currHour*30)+((currMin-1)*0.5)
-    rotateImg('hour')
-
-    //construct seconds
-    second = rightNow.getSeconds()
-    secPos = (second-1)*6
-    rotateImg('second')
-}
-
-//rotational positions of the hands
-let secPos = 0
-let minPos = 0
-let hourPos = 0
-
-//Set seconds to 0
-let second = 0
-
-//call the function to start it running
-myClock()
+let clockObj = { hour: new Date().getHours(), minute: new Date().getMinutes(), second: new Date().getSeconds() }
+setInterval(() => {
+    clockObj.second = (clockObj.second + 1) % 60
+    if (clockObj.second % 60 === 0) {
+        clockObj.minute = (clockObj.minute + 1) % 60
+        if (clockObj.minute % 60 === 0) {
+            clockObj.hour = (clockObj.hour + 1) % 24}}
+    document.getElementById('second').style.transform = `translate(-50%, -50%) rotate(${clockObj.second * 6}deg)`
+    document.getElementById('minute').style.transform = `translate(-50%, -50%) rotate(${clockObj.minute * 6}deg)`
+    document.getElementById('hour').style.transform = `translate(-50%, -50%) rotate(${(clockObj.hour * 30) + (clockObj.minute * 0.5)}deg)`
+}, 1000)
