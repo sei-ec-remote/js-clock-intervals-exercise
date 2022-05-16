294bcbd094b7aab434502d7dbaa7d83e64d9b9fb
tmsheipline <taylorsheipline_2012@hotmail.com>
2022-05-16 19:36:52 -0400

    Styled background, Complete Functionality with UTD time

--- clock.js
+++ clock.js
@@ -1,22 +1,18 @@
+
+// Grab ID's for manipulation
 let secondHand = document.getElementById('second');
 let hourHand = document.getElementById('hour');
 let minuteHand = document.getElementById('minute');
 
-currentTime = new Date();
-console.log(currentTime);
 
-let minutes = currentTime.getMinutes();
-let seconds = currentTime.getSeconds();
-let hours = currentTime.getHours();
-
-
-
-
-const runClock = () =>{
-    console.log(seconds);
-    secondHand.style.transform = "rotate(" + (seconds / 60 * 360) + "deg)";
-    hourHand.style.transform = "rotate(" + (hours / 12 * 360) + "deg)";
-    minuteHand.style.transform = "rotate(" + (minutes / 60 * 360) + "deg)";
+const runClock = () => {
+    const currentTime = new Date();
+    let minutes = currentTime.getMinutes();
+    let seconds = currentTime.getSeconds();
+    let hours = currentTime.getHours();
+    secondHand.style.transform = "rotate(" + ((seconds / 60) * 360) + "deg)";
+    hourHand.style.transform = "rotate(" + ((hours / 12) * 360) + "deg)";
+    minuteHand.style.transform = "rotate(" + ((minutes / 60) * 360) + "deg)";
 };
 
 setInterval(runClock, 1000);
