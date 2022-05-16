c65fb4e9bcce13a807a2d9fa9ec7c5c9b24736ed
“kmcaloney15” <Katherine@Katys-MBP-2.hsd1.ma.comcast.net>
2022-05-16 18:23:43 -0400

    Was able to get all of the clock hands to overlay over the clock

--- clock.js
+++ clock.js
@@ -1,18 +1,28 @@
 //okay lets write out what I want to code
 //we need to make the clock have an hour, minute and red second hand
 
+//setting the current time
+currentTime = new Date();
+console.log(currentTime);
+
+
+
+//want to make the minutes go from 0 to 60. 
+
+
+
+
 // Create intervals in the JavaScript file to make the clocks tick.
-
-
-
-// Change CSS values in JavaScript by getting reference to them through the DOM and setting a new value to their CSS properties:
 
 let degrees = 45;
 
 
 //pulling the hour info
+//set the degree that it should start at
 const hourElement = document.getElementById("hour")
-el.style.transform = "rotate(" + degrees + "deg)"
+hourElement.style.transform = "rotate(" + degrees + "deg)"
+
+// Change CSS values in JavaScript by getting reference to them through the DOM and setting a new value to their CSS properties:
 
 // It would be useful to define functions that convert seconds, minutes and hours to how many degrees the hand should be rotated around the clock!
 
@@ -25,11 +35,6 @@ el.style.transform = "rotate(" + degrees + "deg)"
 
 // hourRotation(12) // returns 0
 // hourRotation(5) // returns (5 / 12) * 360
-
-
-currentTime = new Date();
-console.log(currentTime);
-
 
 
 
