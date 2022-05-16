f2ded6844a937fbeb9ba7b355a1dc9f00d57007f
jhimson <jhimsonrayraboypamisa@gmail.com>
2022-05-16 17:39:54 -0400

    Add hourRotation function to convert currentHour into degree value

--- clock.js
+++ clock.js
@@ -4,3 +4,6 @@ const seconds = document.getElementById('second');
 const minutes = document.getElementById('minute');
 // Getting the image element of hour by id and storing it the variable hours.
 const hours = document.getElementById('hour');
+
+// Hours calculator to convert current hour to exact degrees position of the hours hand.
+const hourRotation = currentHour => (currentHour / 12) * 360;
