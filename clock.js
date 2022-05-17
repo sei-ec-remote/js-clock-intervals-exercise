729e32759c292ba68cb6965f391cdf9657d33327
mileslathan <mlathan.cn@gmail.com>
2022-05-16 21:54:45 -0400

    iter.2: Added variables to grab html elements and added counters for each.

---
+++ clock.js
@@ -0,0 +1,11 @@
+//Pulling elements from the HTML into js variables.
+const hoursIMG = document.getElementById("hour");
+const minutesIMG = document.getElementById("minute");
+const secondsIMG = document.getElementById("second")
+console.log(hoursIMG);
+
+
+//Adding counters that will represent the time that passes.
+let countHours = 0;
+let countMinutes = 0;
+let countSeconds = 0;
