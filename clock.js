3f09d1bf074863e7b4f2a848ac3a38b61a5012cf
patrickbasham <PatrickTBasham@gmail.com>
2022-05-16 23:23:32 -0400

    completed basic clock

---
+++ clock.js
@@ -0,0 +1,44 @@
+const clock_second = document.getElementById("second");
+const clock_minute = document.getElementById("minute");
+const clock_hour = document.getElementById("hour");
+
+// create interval for hour
+//interval for minute
+// interval for second
+let secondCount = 0;
+let minuteCount = 0;
+let hourCount = 0;
+const intervalFunction = () => {
+    myCounter = setInterval(() => {
+        secondCount++;
+        // update seconds hand
+        clock_second.style.transform = "rotate(" + secondRotation(secondCount) + "deg)";
+        if (secondCount % 60 === 0) {
+            minuteCount++;
+            console.log(minuteCount);
+            console.log("Move minute hand");
+            clock_minute.style.transform = "rotate(" + minuteRotation(minuteCount) + "deg)";
+        }
+        if (minuteCount % 60 === 0 && minuteCount != 0 && secondCount % 60 === 0) {
+            hourCount++;
+            console.log(hourCount);
+            console.log("Move hour hand");
+            clock_hour.style.transform = "rotate(" + hourRotation(hourCount) + "deg)";
+        }
+        
+        
+        console.log(secondCount)
+    }, 1000)
+}
+
+const secondRotation = (seconds) => {
+    return (seconds / 60) * 360
+}
+const minuteRotation = (minute) => {
+    return (minute / 60) * 360
+}
+const hourRotation = (hour) => {
+    return (hour / 12) * 360
+}
+
+intervalFunction();
