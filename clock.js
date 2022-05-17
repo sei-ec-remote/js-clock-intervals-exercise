ad805f03cfd1d5867d4960a2804b40a77c720de6
annakoanna <anna.231991k@gmail.com>
2022-05-16 23:12:15 -0700

    Fixed first part of function clock

--- clock.js
+++ clock.js
@@ -1,9 +1,9 @@
 function clock() {
 let degrees = 6;
-const secondsArray = document.getElementById("minute")
-const minutesArray = document.getElementById("second")
+const secondsArray = document.getElementById("second")
+const minutesArray = document.getElementById("minute")
 const hoursArray = document.getElementById("hour")
-const el = document.getElementById("rotate")
+//const el = document.getElementById("rotate")
 
 
 setInterval(() => {
@@ -12,11 +12,10 @@ const hours = day.getMinutes() * 30;
 const minutes = day.getMinutes() * degrees;
 const seconds = day.getSeconds() * degrees;
 
-//hel.style.transform = "rotate(" + degrees + "deg)"
+"
 hoursArray.style.transform = `rotateZ(${hours + (minutes /12)}degrees)`
 minutesArray.style.transform = `rotateZ(${minutes}degrees)`
 minutesArray.style.transform = `rotateZ(${seconds}degrees)`
-
 
 }, 0)
 
