b5ce7cfbe56d7e082e55f540d180d3f262750150
bhenderson93 <brycehenderson1993@gmail.com>
2022-05-17 06:30:24 -0700

    Restructured setInterval to be more efficient. Fewer refreshes on the transforms

--- clock.js
+++ clock.js
@@ -1,11 +1,11 @@
 let clockObj = { hour: new Date().getHours(), minute: new Date().getMinutes(), second: new Date().getSeconds() }
 setInterval(() => {
     clockObj.second = (clockObj.second + 1) % 60
+    document.getElementById('second').style.transform = `translate(-50%, -50%) rotate(${clockObj.second * 6}deg)`
     if (clockObj.second % 60 === 0) {
         clockObj.minute = (clockObj.minute + 1) % 60
+        document.getElementById('minute').style.transform = `translate(-50%, -50%) rotate(${clockObj.minute * 6}deg)`
         if (clockObj.minute % 60 === 0) {
             clockObj.hour = (clockObj.hour + 1) % 24}}
-    document.getElementById('second').style.transform = `translate(-50%, -50%) rotate(${clockObj.second * 6}deg)`
-    document.getElementById('minute').style.transform = `translate(-50%, -50%) rotate(${clockObj.minute * 6}deg)`
-    document.getElementById('hour').style.transform = `translate(-50%, -50%) rotate(${(clockObj.hour * 30) + (clockObj.minute * 0.5)}deg)`
+            document.getElementById('hour').style.transform = `translate(-50%, -50%) rotate(${(clockObj.hour * 30) + (clockObj.minute * 0.5)}deg)`
 }, 1000)
