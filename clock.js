2375b974651a5945a48c9a3be426154e3f7f8e75
Wenling Yu <yuwenlingevelyn@gmail.com>
2022-05-16 20:36:38 -0400

    show actual time

--- clock.js
+++ clock.js
@@ -9,24 +9,11 @@ const minuteEl = document.getElementById("minute");
 const secondEl = document.getElementById("second");
 const rotationEl = document.querySelectorAll(".rotate");
 
-//reset degrees - not working and no need to reset?
-// if ((secDegrees = 60)) secDegrees = 0;
-// if ((minDegrees = 60)) minDegrees = 0;
-// if ((hourDegrees = 12)) hourDegrees = 0;
-
-//create time intervals
-const secondInterval = setInterval(() => {
-  rotationEl[2].style.transform = `rotate(${secondRotation(secDegrees)}deg)`;
-  secDegrees++;
-}, 1000);
-const minuteInterval = setInterval(() => {
-  minDegrees++;
-  rotationEl[1].style.transform = `rotate(${minuteRotation(minDegrees)}deg)`;
-}, 60000);
-const hourInterval = setInterval(() => {
-  hourDegrees++;
-  rotationEl[0].style.transform = `rotate(${hourRotation(hourDegrees)}deg)`;
-}, 60000 * 60);
+//grab actual time at the moment
+const now = new Date();
+let second = now.getSeconds();
+let minute = now.getMinutes();
+let hour = now.getHours() % 12;
 
 //functions convert seconds, minutes, hours
 //to show how many degrees the hand should be roated
@@ -41,3 +28,25 @@ const minuteRotation = (ele) => {
 const hourRotation = (ele) => {
   return (ele / 12) * 360;
 };
+
+//need to show up immediately
+rotationEl[2].style.transform = `rotate(${secondRotation(second)}deg)`;
+rotationEl[1].style.transform = `rotate(${minuteRotation(minute)}deg)`;
+rotationEl[0].style.transform = `rotate(${hourRotation(hour)}deg)`;
+
+//create time intervals
+const secondInterval = setInterval(() => {
+  rotationEl[2].style.transform = `rotate(${secondRotation(second)}deg)`;
+  //if more than 60 then reset it to 1
+  second >= 60 ? (second = 1) : second++;
+  console.log(second);
+}, 1000);
+
+const minuteInterval = setInterval(() => {
+  rotationEl[1].style.transform = `rotate(${minuteRotation(minute)}deg)`;
+  minute >= 60 ? (minute = 1) : minute++; //if more than 60 then reset it to 1
+}, 60000);
+const hourInterval = setInterval(() => {
+  rotationEl[0].style.transform = `rotate(${hourRotation(hour)}deg)`;
+  hour >= 12 ? (hour = 1) : hour++; //if more than 12 then reset it to 1
+}, 60000 * 60);
