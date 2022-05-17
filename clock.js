3af8fae4c2fccb75e9c4dce7abf7edd8c6eaad6b
christopher chapa <chapachristopher16@gmail.com>
2022-05-16 21:18:00 -0500

    Complete Clock Deliverable

--- clock.js
+++ clock.js
@@ -9,16 +9,18 @@ hourHand = document.getElementById('hour')
 // 1 hour equals 30 degrees
 // 1 second = 6 degrees
 // 1 minute = 6 degrees
+setInterval(() => {
+    let time = new Date()
+    let oneHour = time.getHours() * 30
+    let oneSecond = time.getSeconds() * 6
+    let oneMinute = time.getMinutes() * 6
+    // access styling for second hand and inputs degrees to rotate
+    secondHand.style.transform = `rotate(${oneSecond}deg)`
 
-let seconds = setInterval(() => {
-    console.log('second')
-},1000)
+    // access styling for minute hand and inputs degrees to rotate
+    minuteHand.style.transform = `rotate(${oneMinute}deg)`
 
-let minutes = setInterval(() => {
-    console.log('minute')
-},60000)
-
-let hour = setInterval(() => {
-    console.log('hour')
-},3600000)
-console.log(seconds)
+    //  access styling for hour hand and inputs degrees to rotate
+    hourHand.style.transform = `rotate(${oneHour}deg)`
+})
+// I used MDN to help me understand how to better access the time rather than setting individual setIntervals in 1,000 60,000 and 3,600,000
