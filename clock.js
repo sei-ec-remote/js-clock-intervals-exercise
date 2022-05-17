a3adf2b95d4cf3c917c0767f56e83144f1867695
peterxu35 <peter.px.xu@gmail.com>
2022-05-17 01:58:01 -0400

    fixed

--- clock.js
+++ clock.js
@@ -5,21 +5,21 @@ const secondHand = document.getElementById("second")
 
 let degree = 0
 function rotateSeconds(){
-    secondHand.style.transform = "rotate(" + degree + "deg)"
     degree += 6
+    secondHand.style.transform = "rotate(" + degree + "deg)"
 }
 
 let degree1 = 0
 function rotateMinutes(){
-    minuteHand.style.transform = "rotate(" + degree1 + "deg)"
     degree1 += 6
+    minuteHand.style.transform = "rotate(" + degree1 + "deg)"
 }
 
 
 let degree2 = 0
 function rotateHours(){
-    hourHand.style.transform = "rotate(" + degree2 + "deg)"
     degree2 += 30
+    hourHand.style.transform = "rotate(" + degree2 + "deg)"
 }
 setInterval(rotateSeconds, 1000)
 setInterval(rotateMinutes, 60000)
