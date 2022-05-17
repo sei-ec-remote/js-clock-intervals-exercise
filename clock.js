9dd514783d39a729ef1243cfa0f03541cdfa8865
patwyzlic <patrickwyzlic@gmail.com>
2022-05-16 20:13:16 -0400

    Commit 3 - Calcs of Time, Current Date

--- clock.js
+++ clock.js
@@ -1,4 +1,5 @@
 let degrees = 45;
+let count = 0;
 
 const hourEl = document.getElementById("hour");
 hourEl.style.transform = "rotate(" + degrees + "deg)";
@@ -9,3 +10,23 @@ minuteEl.style.transform = "rotate(" + degrees + "deg)";
 const secondEl = document.getElementById("second");
 secondEl.style.transform = "rotate(" + degrees + "deg)";
 
+const hourRotation = (hours) => {
+    return (hours/12) * 360;
+};
+const minuteRotation = (minutes) => {
+    return (minutes/60) * 360;
+};
+const secondRotation = (seconds) => {
+    return (seconds/60) * 360;
+};
+
+
+const date = new Date();
+
+setInterval(() => {
+    count++;
+    console.log(date);
+    console.log(count);
+}, 1000);
+
+console.log(secondRotation(59));
