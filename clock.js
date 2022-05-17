c2839efda5d650c08cb1c8facd813af2b2dd6a38
hnhtran <tran_eetas@yahoo.com>
2022-05-17 23:23:17 +1000

    Set up the hour, minute, and second arm in the picture

---
+++ clock.js
@@ -0,0 +1,12 @@
+//to make the clock tick
+// .rotate {
+//     transform: rotate(45deg)
+// }
+
+//Change CSS values in JavaScript by getting reference to them through the DOM and setting a new value to their CSS properties:
+// let degrees = 45;
+
+// const el = document.getElementById("myElement")
+// el.style.transform = "rotate(" + degrees + "deg)"
+
+
