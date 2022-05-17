c09f3d3f384df414a910d4edffdf336b0c0e1b12
earlhalasan <earl.halasan@gmail.com>
2022-05-16 23:31:47 -0400

    Attempted to make it display real time. Noted out the code below

--- clock.js
+++ clock.js
@@ -25,5 +25,33 @@ const hoursFunction = setInterval(() => {
   hoursHand = hoursHand + 1;
   hoursDegrees = (hoursHand / 216000) * 360;
   hoursEl.style.transform = "rotate(" + hoursDegrees + "deg)";
-  console.log(minutesHand);
+  console.log(hoursHand);
 }, 1000);
+
+// // ATTEMPTED REAL TIME
+// const now = new Date();
+// const secondEl = document.getElementById("second");
+// const minuteEl = document.getElementById("minute");
+// const hoursEl = document.getElementById("hour");
+
+// // Functions for hands
+// const secondsFunction = setInterval(() => {
+//   secondsHand = now.getSeconds() + 1;
+//   secondDegrees = (secondsHand / 60) * 360;
+//   secondEl.style.transform = "rotate(" + secondDegrees + "deg)";
+//   console.log(secondsHand);
+// }, 1000);
+
+// const minutesFunction = setInterval(() => {
+//   minutesHand = now.getMinutes() + 1;
+//   minutesDegrees = (minutesHand / 3600) * 360;
+//   minuteEl.style.transform = "rotate(" + minutesDegrees + "deg)";
+//   console.log(minutesHand);
+// }, 1000);
+
+// const hoursFunction = setInterval(() => {
+//   hoursHand = now.getHours() + 1;
+//   hoursDegrees = (hoursHand / 216000) * 360;
+//   hoursEl.style.transform = "rotate(" + hoursDegrees + "deg)";
+//   console.log(hoursHand);
+// }, 1000);
