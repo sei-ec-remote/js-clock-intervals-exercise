8592da9d9fb35d59a48d917816a2390a11a19e1a
Freightscape <jordan@freightscape.net>
2022-05-17 04:47:37 -0400

    Set intervals for the Hands

--- clock.js
+++ clock.js
@@ -1,2 +1,12 @@
 //just forked Repo and cloned to Machine starting @6:15pm
+//console.log("Getting Started, Tiras P. Jordan can do this Coding thing, he's very good at it")
+//console.log("hey Colette")
 
+//Starting back up at 4:35am
+
+//1. Create intervals in the JavaScript file to make the clocks tick.
+// found resource from an existing codepen: https://codepen.io/sean_cotton/pen/rvpbQz
+
+setInterval(() => {
+    let minute = document.getElementById("minute")
+});
