c549fe6bd14b1e675cc224b3542301aa2511dfb8
annakoanna <anna.231991k@gmail.com>
2022-05-16 23:40:22 -0700

    Last version of clock

--- clock.js
+++ clock.js
@@ -3,22 +3,27 @@ let degrees = 6;
 const secondsArray = document.getElementById("second")
 const minutesArray = document.getElementById("minute")
 const hoursArray = document.getElementById("hour")
-//const el = document.getElementById("rotate")
-
+//const el = document.getElementById("rotate
 
 setInterval(() => {
 const now = new Date();
 const secondDegree = secondRotation(now.getSeconds());
 const minuteDegree = minuteRotation(now.getMinutes());
-const hourDegreem = hourRotation(now.getHours(),now.getMinutes());
-
-
-
-
+const hourDegree = hourRotation(now.getHours(), now.getMinutes());
 
 hoursArray.style.transform = `rotate(${hourDegree}deg)`;
 minutesArray.style.transform = `rotate(${minuteDegree}deg)`;
-minutesArray.style.transform = `rotate(${secondDegree}deg)`;
+secondsArray.style.transform = `rotate(${secondDegree}deg)`;
 }, 1000)
 }
+
 clock();
+function secondRotation(second) {
+    return second *6;
+}
+function minuteRotation(minute) {
+    return minute * 6;
+}
+function hourRotation(hour, minute) {
+    return (hour % 12 + minute/60) * 30;
+}
