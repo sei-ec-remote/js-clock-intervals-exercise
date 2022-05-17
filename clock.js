591012e93c6d80dd5f9080f21470398ee9545732
patwyzlic <patrickwyzlic@gmail.com>
2022-05-16 20:43:24 -0400

    Commit 5 - Continuing to find time

--- clock.js
+++ clock.js
@@ -1,16 +1,22 @@
-let degrees = 0;
+let degreesHours = 0;
+let degreesMinutes = 0;
+let degreesSeconds = 0;
 let count = 0;
 
 const hourEl = document.getElementById("hour");
-hourEl.style.transform = "rotate(" + degrees + "deg)";
+hourEl.style.transform = "rotate(" + degreesHours + "deg)";
 
 const minuteEl = document.getElementById("minute");
-minuteEl.style.transform = "rotate(" + degrees + "deg)";
+minuteEl.style.transform = "rotate(" + degreesMinutes + "deg)";
 
 const secondEl = document.getElementById("second");
-secondEl.style.transform = "rotate(" + degrees + "deg)";
+secondEl.style.transform = "rotate(" + degreesSeconds + "deg)";
 
 const hourRotation = (hours) => {
+    if(hours > 12){
+        hours -= 12;
+        Math.abs(hours); 
+    }
     return (hours/12) * 360;
 };
 const minuteRotation = (minutes) => {
@@ -20,19 +26,19 @@ const secondRotation = (seconds) => {
     return (seconds/60) * 360;
 };
 
-const allTime = (amountRotation, getTime) => {
-    return amountRotation(getTime);
-}
-
-allTime(hourRotation, Date.prototype.getHours());
-allTime(minuteRotation, Date.prototype.getHours());
-allTime(secondRotation, Date.prototype.getHours());
-
-
 const date = new Date();
 
+const allTime = () => {
+    degreesHours = date.getHours();
+    degreesMinutes = date.getMinutes();
+    degreesSeconds = date.getSeconds();
+    hourRotation(degreesHours);
+    minuteRotation(degreesMinutes);
+    secondRotation(degreesSeconds);
+}
+
 setInterval(() => {
+    allTime();
     count++;
-    console.log(date);
     console.log(count);
 }, 1000);
