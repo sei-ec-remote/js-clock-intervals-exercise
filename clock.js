1354dbc645119f97a26b12f82752b0054e62c6b3
patrickbasham <PatrickTBasham@gmail.com>
2022-05-17 09:29:07 -0400

    Updated clock with live time

--- clock.js
+++ clock.js
@@ -2,43 +2,48 @@ const clock_second = document.getElementById("second");
 const clock_minute = document.getElementById("minute");
 const clock_hour = document.getElementById("hour");
 
-// create interval for hour
-//interval for minute
-// interval for second
-let secondCount = 0;
-let minuteCount = 0;
-let hourCount = 0;
-const intervalFunction = () => {
-    myCounter = setInterval(() => {
-        secondCount++;
-        // update seconds hand
-        clock_second.style.transform = "rotate(" + secondRotation(secondCount) + "deg)";
-        if (secondCount % 60 === 0) {
-            minuteCount++;
-            console.log(minuteCount);
-            console.log("Move minute hand");
-            clock_minute.style.transform = "rotate(" + minuteRotation(minuteCount) + "deg)";
-        }
-        if (minuteCount % 60 === 0 && minuteCount != 0 && secondCount % 60 === 0) {
-            hourCount++;
-            console.log(hourCount);
-            console.log("Move hour hand");
-            clock_hour.style.transform = "rotate(" + hourRotation(hourCount) + "deg)";
-        }
-        
-        
-        console.log(secondCount)
-    }, 1000)
+
+const clock = () => {
+    const currentTime = new Date();
+    
+    let secondCount = currentTime.getSeconds();
+    let minuteCount = currentTime.getMinutes();
+    let hourCount = currentTime.getHours();
+    clock_second.style.transform ="rotate(" + ((secondCount / 60) * 360) + "deg)";
+    clock_minute.style.transform ="rotate(" + ((minuteCount / 60) * 360) + "deg)";
+    clock_hour.style.transform ="rotate(" + ((hourCount / 12) * 360) + "deg)";
 }
 
-const secondRotation = (seconds) => {
-    return (seconds / 60) * 360
-}
-const minuteRotation = (minute) => {
-    return (minute / 60) * 360
-}
-const hourRotation = (hour) => {
-    return (hour / 12) * 360
-}
+// const intervalFunction = () => {
+//     myCounter = setInterval(() => {
+//         secondCount++;
+//         // update seconds hand
+//         clock_second.style.transform = "rotate(" + secondRotation(secondCount) + "deg)";
+//         if (secondCount % 60 === 0) {
+//             minuteCount++;
+//             console.log(minuteCount);
+//             console.log("Move minute hand");
+//             clock_minute.style.transform = "rotate(" + minuteRotation(minuteCount) + "deg)";
+//         }
+//         if (minuteCount % 60 === 0 && minuteCount != 0 && secondCount % 60 === 0) {
+//             hourCount++;
+//             console.log(hourCount);
+//             console.log("Move hour hand");
+//             clock_hour.style.transform = "rotate(" + hourRotation(hourCount) + "deg)";
+//         } 
+//         console.log(secondCount)
+//     }, 1000)
+// }
 
-intervalFunction();
+// const secondRotation = (seconds) => {
+//     return (seconds / 60) * 360
+// }
+// const minuteRotation = (minute) => {
+//     return (minute / 60) * 360
+// }
+// const hourRotation = (hour) => {
+//     return (hour / 12) * 360
+// }
+
+// intervalFunction();
+setInterval(clock, 1000);
