3c49d8f51c59e3519eb87fbfd18815870c63f829
jrbreeden <jrbreeden55@gmail.com>
2022-05-18 15:28:57 -0400

    made clock hands move

--- clock.js
+++ clock.js
@@ -1,24 +1,30 @@
-setInterval(setClock, 1000)
+let date = new Date();
+console.log(date);
 
-const hourHand = document.querySelector('[data-hour-hand]')
-const minuteHand = document.querySelector('[data-minute-hand]')
-const secondHand = document.querySelector('[data-second-hand]')
 
-function setClock() {
-  const currentDate = new Date()
-  const secondsRatio = currentDate.getSeconds() / 60
-  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
-  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
-  setRotation(secondHand, secondsRatio)
-  setRotation(minuteHand, minutesRatio)
-  setRotation(hourHand, hoursRatio)
-}
-let degrees = 45;
+const secHand = document.getElementById("second");
+let sec = date.getSeconds ();
+let degS =sec * 6;
+setInterval(function(){
+    secHand.style.transform="rotate(" +degS + "deg)";
+    degS=(degS+1)
+},166.6666666666666666666666666666);
 
-const el = document.getElementById("myElement")
-el.style.transform = "rotate(" + degrees + "deg)"
-function setRotation(element, rotationRatio) {
-  element.style.setProperty('--rotation', rotationRatio * 360)
+const minHand = document.getElementById("minute");
+let min = date.getMinutes ();
+let degM=min*6+sec/12;
+setInterval(function(){
+    minHand.style.transform="rotate(" + degM + "deg)";
+    degM=(degM+.1)
+},1000);
 
-  setClock() 
-}
+const hrHand = document.getElementById("hour");
+let hour = date.getHours ();
+let degH=hour*10+min/12+60;
+
+setInterval(function(){
+
+    hrHand.style.transform="rotate(" +degH + "deg)";
+    degH=(degH+.01)
+
+},1200);
