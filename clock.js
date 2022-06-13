82d0ff822469cae269232da679b1d67352bf00da
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2022-06-13 17:02:01 -0400

    almost done.

---
+++ clock.js
@@ -0,0 +1,42 @@
+let degrees = 45; 
+const face = document.getElementById("face")
+const secHand = document.getElementById("second")
+const minHand = document.getElementById("minute")
+const hrHand = document.getElementById("hour")
+
+const secondHand = () => {
+    let time = 1;
+    const sHandMover = () => {
+        secHand.style.transform = "rotate(" + (time/60)*360 +
+         "deg)"
+        time++
+    }
+    const secondHandTimer = setInterval(sHandMover, 1000)
+}
+
+secondHand()
+
+const minuteHand = () => {
+    let time = 1;
+    const mHandMover = () => {
+        minHand.style.transform = "rotate(" = (time/60)*360 +
+        "deg)"
+       time++
+    }
+    const minuteHandTimer = setInterval(mHandMover, 60000)
+}
+
+hourHand()
+
+const hourHand = () => {
+    let time = 1;
+    const sHandMover = () => {
+        secHand.style.transform = "rotate(" + (time/60)*360 +
+         "deg)"
+        time++
+    }
+    const secondHandTimer = setInterval(sHandMover, 360000)
+}
+
+hourHand()
+
