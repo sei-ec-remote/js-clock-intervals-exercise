364658b3c66e90bdf9aff30b584dd6865e9e6f9c
Alys <alice.g.cooper@gmail.com>
2022-06-13 14:09:11 -0400

    Add pseudocode to clock.js

---
+++ clock.js
@@ -0,0 +1,21 @@
+// need to create function
+// that takes interval of 1000ms and rotates the 
+// second hand by 6 degrees (takes (currentSecond * 6 + 'deg')
+// and sets the current rotate class transform to that degree
+
+
+// set seconds for current Date
+// this function will need to run every second
+console.log(new Date())
+let sec = new Date().getSeconds();
+console.log(sec);
+let min = new Date().getMinutes();
+console.log(min);
+// will need to, if hour exceeds 12, subtract 12 (24h time)
+let hour = new Date().getHours();
+console.log(hour);
+
+// const updateTime
+
+
+
