27bf91590eb2f5f9233a52d89189df34e754573f
SunGod7 <sun7ruler@gmail.com>
2022-06-24 17:17:22 -0400

    finished

--- clock.js
+++ clock.js
@@ -1,29 +1,34 @@
-// // // // // //Create intervals in the JavaScript file to make the clocks tick.
-// // // // // //.rotate {
-// // // // //    // transform: rotate(45deg)
-// // // // // //}
+// // // // // // //Create intervals in the JavaScript file to make the clocks tick.
+// // // // // // //.rotate {
+// // // // // //    // transform: rotate(45deg)
 
 
+const hourHand = document.getElementById("hour")
+const minuteHand = document.getElementById("minute")   
+const secondHand = document.getElementById("second")
 
+let timerInterval;
 
- let Second = document.getElementById("second");
- //let timeInSeconds = 0
-//let timeInMinutes = 0
-    const setClock = () => {
-        // console.log(timeInSeconds)
-        //timeInSeconds++
-         const day = new Date();
-        const ss = day.getSeconds();
-        
-         const seconddeg = (ss + 6);
-        
-        second.style.transform = `rotate(${secondDeg}deg)`;
-    }
-    setClock();
-     const intervalTimer = setInterval(setClock, 1000)
-
-
+const initClock = () => {
+    const date = new Date();
     
-
-
+    const seconds = date.getSeconds()
+    const minutes = date.getMinutes()
+    const hours = date.getHours()
     
+    
+    const secondsDegrees =  6 * seconds;
+    const minutesDegrees = 6 * minutes;
+    const hoursDegrees = 30 * hours + minutes/2;
+    
+    
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+       
+    
+    
+}
+setInterval(initClock, 1000);
+   
+console.log(initClock())//clearInterval(timerInterval);
