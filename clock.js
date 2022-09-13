fe86b73c0e980376acc2a383bb665ad94a6e65cd
skbarbee <barbee.s.k@gmail.com>
2022-09-13 17:02:45 -0400

    laid out the functions and created variables

---
+++ clock.js
@@ -0,0 +1,15 @@
+const secondHand = document.querySelector("#second")
+const minuteHand = document.querySelector("#minute")
+const hourHand = document.querySelectorAll("#hour")
+const clockFace = document.querySelector("#face")
+
+const secondHandRotation = ()=>{
+
+}
+const minuteHandRotation = ()=>{
+    
+}
+
+const hourHandRotation = ()=>{
+    
+}
