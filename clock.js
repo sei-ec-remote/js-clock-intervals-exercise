71213e1004b1f49bbaab6d52f9d6c84746172389
skbarbee <barbee.s.k@gmail.com>
2022-09-13 22:00:09 -0400

    formated function for digital clock

--- clock.js
+++ clock.js
@@ -1,15 +1,52 @@
 const secondHand = document.querySelector("#second")
 const minuteHand = document.querySelector("#minute")
-const hourHand = document.querySelectorAll("#hour")
+const hourHand = document.querySelector("#hour")
 const clockFace = document.querySelector("#face")
+const digitalHour = document.querySelector("#dhour")
 
-const secondHandRotation = ()=>{
+const digitalMinute = document.querySelector("#dminute")
+
+const setDate = ()=> {
+const now = new Date()
+//second hand rotation
+const seconds = now.getSeconds()
+const secondsDegrees = ((seconds/60) * 360)
+//console.log(seconds)
+secondHand.style.transform =`rotate(${secondsDegrees}deg)`
+//minute hand rotation
+const minutes = now.getMinutes()
+const minuteDegrees = ((minutes/60) * 360)
+//console.log(minutes)
+minuteHand.style.transform = `rotate(${minuteDegrees}deg)`
+// hour hand rotation
+const hours = now.getHours()
+const hourDegrees = ((hours/12) * 360)
+hourHand.style.transform = `rotate(${hourDegrees}deg)`
+//console.log(hourDegrees)
+
+
+
 
 }
-const minuteHandRotation = ()=>{
-    
-}
+setInterval(setDate,1000)
 
-const hourHandRotation = ()=>{
-    
+const makeDigitalClock = () => {
+    const now = new Date()
+    let hours = now.getHours()
+    const minutes = now.getMinutes()
+    let digitalClock = document.createElement("h1")
+
+if (hours <= 12){
+    digitalClock.textContent = `${hours} : ${minutes} AM`}
+    else{
+        hours = hours - 12
+        digitalClock.textContent = `${hours} : ${minutes} PM`}
+
+const dClock = document.getElementById('digital')
+dClock.appendChild(digitalClock)
+
+digitalClock.style.fontSize = "72px"
+
+digitalClock.style.textAlign = "center"
 }
+makeDigitalClock()
