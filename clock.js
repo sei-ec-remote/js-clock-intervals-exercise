2d100997a9d6d93cd99b821171eaab28c9e04b75
estebbins <stebbins.e@gmail.com>
2022-12-19 17:55:11 -0500

    Finished deliverable

--- clock.js
+++ clock.js
@@ -1,41 +1,44 @@
 // Write a function that rotates the hands every interval
+
+// Get HTML elements & set as variables
 const secondHand = document.getElementById('second')
 const minuteHand = document.getElementById('minute')
 const hourHand = document.getElementById('hour')
 
-
+// establish a count
 let count = 0
 
 const countUp = () => {
+    // Increase the count each time the function is called
     count++
     return count
 }
 
-const secRot = (milliseconds) => {
+// Establish increments  of degrees for seconds
+const secRot = () => {
     const degrees = 360/60
     return degrees
 }
 
-const minRot = (milliseconds) => {
+// Establish increments  of degrees for minutes
+const minRot = () => {
     const degrees = (360/60)/60
     return degrees
 }
-
-const hrRot = (milliseconds) => {
+// Establish increments  of degrees for hours
+const hrRot = () => {
     const degrees = (360/(60*60))/60
     return degrees
 }
 
-// can you pass a function that requires a parameter to setTimeout and setInterval
-
-
-// need to increment the rotation.
-rotateClock = (milliseconds) => {
+// Rotate the clock
+rotateClock = () => {
+    // count up once each interval & rotate the hands by their respective degrees, for every count increase.
     countUp()
-    secondHand.style.transform = 'rotate(' + secRot(milliseconds)*count + 'deg)'
-    minuteHand.style.transform = 'rotate(' + minRot(milliseconds)*count + 'deg)'
-    hourHand.style.transform = 'rotate(' + hrRot(milliseconds)*count + 'deg)'
+    secondHand.style.transform = 'rotate(' + secRot()*count + 'deg)'
+    minuteHand.style.transform = 'rotate(' + minRot()*count + 'deg)'
+    hourHand.style.transform = 'rotate(' + hrRot()*count + 'deg)'
 }
 
 
-setInterval(rotateClock, 1000, 1000)
+setInterval(rotateClock, 1000)
