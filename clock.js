419dd858be1b2621a95d4f879d2cef9d051e19a8
isobelmorales <isofmorales@gmail.com>
2022-12-19 19:14:40 -0500

    Completed Deliverable

---
+++ clock.js
@@ -0,0 +1,33 @@
+// Grabbing DOM Elements
+const secondHand = document.querySelector('#second')
+const minuteHand = document.querySelector('#minute')
+const hourHand = document.querySelector('#hour')
+
+// Functions to change hand rotations
+const secondRotation = (now) => {
+    let currentSecond = now.getSeconds()
+    secondHand.style.transform = 'rotate(' + (currentSecond * 6) + 'deg)'
+    console.log(`rotate(${currentSecond * 6}deg)`)
+}
+
+const minuteRotation = (now) => {
+    let currentMinute = now.getMinutes()
+    minuteHand.style.transform = 'rotate(' + (currentMinute * 6) + 'deg)'
+}
+
+const hourRotation = (now) => {
+    let currentHour = now.getHours()
+    hourHand.style.transform = 'rotate(' + (currentHour * 30) + 'deg)'
+}
+
+// Interval Functions
+const setClock = () => {
+    let now = new Date()
+    secondRotation(now)
+    minuteRotation(now)
+    hourRotation(now)
+}
+
+setInterval(setClock, 1000)
+
+document.addEventListener('DOMContentLoaded', setClock)
