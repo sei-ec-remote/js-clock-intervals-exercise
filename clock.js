807ecccc2e76f80715293df9ecbb2b2e86c931d9
thepjos <josh.thephasdin@gmail.com>
2022-12-19 20:56:48 -0600

    committing deliverable, clock done, timer on left showing but not working

--- clock.js
+++ clock.js
@@ -1,15 +1,33 @@
+
+
 // give the hands a CLASS of rotate
 //rotate them clockwise. css till do 45 degrees. do seconds every second. minute every 60 seconds. hour every 3600 seconds.
-
-
 
 //write a function that rotates them at intervals
 
 // each hand moves a certain % of 360
-const oneMove = (360 / 60)
+const oneQuarterMove = ((360 / 60) / 4)
+const oneMove = (360/ 60)
 
+let hourDegrees = 1.5
+const rotateHour= () => {
+    const hourHand = document.getElementById('hour')
+    hourHand.style.transform = "rotate(" + hourDegrees + "deg)"
+    hourDegrees = hourDegrees + oneQuarterMove
+}
+//shoudl be 360000
+setInterval(rotateHour, 90000)
 
-let secDegrees = 0
+let minDegrees = 1.5
+const rotateMinute= () => {
+    const minuteHand = document.getElementById('minute')
+    minuteHand.style.transform = "rotate(" + minDegrees + "deg)"
+    minDegrees = minDegrees + oneQuarterMove
+}
+// shoudl be 60000
+setInterval(rotateMinute,15000)
+
+let secDegrees = 6
 const rotateSecond = () => {
     const secondHand = document.getElementById('second')
     secondHand.style.transform = "rotate(" + secDegrees + "deg)"
@@ -17,23 +35,27 @@ const rotateSecond = () => {
 }
 setInterval(rotateSecond,1000)
 
-let minDegrees = 0
-const rotateMinute= () => {
-    const minuteHand = document.getElementById('minute')
-    minuteHand.style.transform = "rotate(" + minDegrees + "deg)"
-    minDegrees = minDegrees + oneMove
-}
-// shoudl be 60000
-setInterval(rotateMinute,60000)
+//const now = new Date()
 
-let hourDegrees = 0
-const rotateHour= () => {
-    const hourHand = document.getElementById('hour')
-    hourHand.style.transform = "rotate(" + hourDegrees + "deg)"
-    hourDegrees = hourDegrees + oneMove
+const date = new Date()
+date.setHours(12,0,0,0)
+
+const currentTime = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds()
+
+const newTime = document.createElement('div')
+newTime.setAttribute('id','digitalClock')
+
+const runningClock = document.querySelector('#clockTime')
+runningClock.appendChild(newTime)
+
+const addTimeToClock = () => {
+    newTime.textContent= currentTime
 }
-//shoudl be 36000000
-setInterval(rotateHour, 360000)
+
+runningClock.addEventListener("load", addTimeToClock())
+
+
+
 
 
 
